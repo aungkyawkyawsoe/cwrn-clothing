219aee02b6e753bda533af872ba37089adffa52f
aungkyawkyawsoe <akks.ygn@gmail.com>
2019-12-01 22:41:11 +0630

    Modified README.md

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 ## Crown Clothing E-commerce
 version 1.0.0
-    
-    
+
+---
+
+### `Reselect` for Redux store
+Firstly we need to install npm package `reselect` by using this command 
+
+- `npm i reselect --save`     
+
+And then `createSelector` for select the redux storage
+
+- `import { createSelector } from 'reselect';`
+
+To Select the state
+
+- `const selectCart = state => state.cart;`
+
+Export structured data, You can destructure anything you like.
+- `export const selectCartHidden = createSelector(
+    [selectCart],
+    cart => cart.hidden
+);`
+
+
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/FirebaseUtils';
 import './Header.scss';
 import CartIcon from '../CartIcon/CartIcon';
 import CartDropdown from '../CartDropdown/CartDropdown';
+import { selectCartHidden } from '../../redux/selector/cartSelector';
+import { selectCurrentUser } from '../../redux/selector/userSelector';
+
 
 
 
@@ -35,9 +39,9 @@ const Header = ({ currentUser, hidden }) => {
     )
 }
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser,
-    hidden: state.cart.hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser(state),
+    hidden: selectCartHidden(state)
 });
 
 
